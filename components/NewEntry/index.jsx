6c3345ba2d301d73f496d1876432d9e604b0a938
Amalia Bryant <amaliabryant@Amalias-MacBook-Air.local>
2021-09-18 16:31:03 -0700

    changed input to useState

--- components/NewEntry/index.jsx
+++ components/NewEntry/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 import HeaderBar from '../HeaderBar';
@@ -7,23 +7,32 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 
 const NewEntry = () => {
+  let [glucoseLvl, setGlucoseLvl] = useState('');
+  let [observedDate, setObservedDate] = useState('');
+  let [observedTime, setObservedTime] = useState('');
   return (
     <React.Fragment>
       <HeaderBar/>
       <Container>
         <h1>New Entry</h1>
         <label>Sugar Level</label>
-        <input type="number" id="glucose_level"/>
+        <input type="number" id="glucose_level" onChange = {(e)=>{
+          setGlucoseLvl(e.target.value);
+        }}/>
         <label>Observation Date</label>
-        <input type="date" id="observed_date"/>
+        <input type="date" id="observed_date" onChange = {(e)=>{
+          setObservedDate(e.target.value);
+        }}/>
         <label>Observation Time</label>
-        <input type="time" id="observed_time"/>
+        <input type="time" id="observed_time" onChange = {(e)=>{
+          setObservedTime(e.target.value);
+        }}/>
         <Button onClick={(e)=>{
           e.preventDefault();
           let entry = {};
-          entry.glucose_level = document.getElementById('glucose_level').value;
-          entry.observed_date = document.getElementById('observed_date').value;
-          entry.observed_time = document.getElementById('observed_time').value;
+          entry.glucose_level = glucoseLvl;
+          entry.observed_date = observedDate;
+          entry.observed_time = observedTime;
 
           // axios.post('/somePath', entry)
           // .then((res)=>{
@@ -33,6 +42,7 @@ const NewEntry = () => {
           //   console.log(err);
           //   alert('Something went wrong');
           // });
+
           //validate the fields
             //if form is filled out correctly
               //send the completed reading record to the db
