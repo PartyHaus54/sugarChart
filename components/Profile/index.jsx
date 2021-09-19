bf76b1b2960bec70c1b1939afd739ead193c6387
Caleb Broderick <liquidarkness1997@gmail.com>
2021-09-18 23:34:42 -0600

    add image to profile

--- components/Profile/index.jsx
+++ components/Profile/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from '../../styles/Profile.module.css';
 import { useRouter } from 'next/router';
 
 import HeaderBar from '../HeaderBar';
@@ -10,10 +11,12 @@ const Profile = () => {
   return (
     <div>
       <HeaderBar/>
-      <h1>Profile</h1>
-      <p>Username</p>
-      <p>DOB</p>
-      <p>Weight</p>
+      <div className={styles.profileContainer}>
+        <img className={styles.profileImage} src='https://cdn.pixabay.com/photo/2016/11/29/20/22/girl-1871104_960_720.jpg'></img>
+        <p>Username</p>
+        <p>DOB</p>
+        <p>Weight</p>
+      </div>
     </div>
   )
 }
