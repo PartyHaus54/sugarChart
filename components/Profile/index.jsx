30f050b41f221f1c323e145b1fbae8e05880e310
Caleb Broderick <liquidarkness1997@gmail.com>
2021-09-19 22:06:21 -0600

    added empty user logic

--- components/Profile/index.jsx
+++ components/Profile/index.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
-<<<<<<< HEAD
 import Link from 'next/link';
 import styles from '../../styles/Profile.module.css';
-=======
->>>>>>> main
 import { useRouter } from 'next/router';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-const Profile = () => {
+const Profile = ({user}) => {
   const router = useRouter();
 
+  user = {userName: 'Chauleb', dob: '05-27-1997', weight: 200}
+
+  if (user) {
+    return (
+      <div>
+        <div className={styles.profileContainer}>
+          {
+            user.image ? <img className={styles.profileImage} src={user.image}></img> :
+            <AccountCircleIcon className = {styles.accountIcon}/>
+          }
+          <p>{user.userName}</p>
+          <p>DOB: {user.dob}</p>
+          <p>Weight: {user.weight}</p>
+        </div>
+      </div>
+  )} else {
   return (
     <div>
-<<<<<<< HEAD
-      <HeaderBar title="Profile"/>
       <div className={styles.profileContainer}>
-        <img className={styles.profileImage} src='https://cdn.pixabay.com/photo/2016/11/29/20/22/girl-1871104_960_720.jpg'></img>
-        <p>Username</p>
-        <p>DOB</p>
-        <p>Weight</p>
+        <img className={styles.profileImage} src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'></img>
+        <p>UserName</p>
+        <p>DOB: 00-00-0000</p>
+        <p>Weight: 0</p>
       </div>
-=======
-      <p>Username</p>
-      <p>DOB</p>
-      <p>Weight</p>
->>>>>>> main
     </div>
-  )
+  )}
 }
 
 export default Profile;
