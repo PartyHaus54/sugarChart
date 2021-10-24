2c99f0fda5abe2337bcefdfaf0c8a388546aff5b
Joseph Peck <joe.r.peck@gmail.com>
2021-10-23 23:35:53 -0700

    SVG ranges displayed. Pseudocoding the calibration process is documented

--- components/Charts/Chart/index.jsx
+++ components/Charts/Chart/index.jsx
@@ -14,6 +14,15 @@ const StyledSVG = styled.svg`
 const Chart = ({timeRange, readings}) => {
   const [chartWidth, setChartWidth] = useState(0);
   const [chartHeight, setChartHeight] = useState(0);
+  const [lowestPoint, setLowestPoint] = useState(70);
+  const [highestPoint, setHighestPoint] = useState(99);
+  const [viewTop, setViewTop] = useState(((highestPoint - lowestPoint) / 10) + highestPoint);
+  const [viewBottom, setViewBottom] = useState(lowestPoint - ((highestPoint - lowestPoint) / 10));
+  const [viewLeft, setViewLeft] = useState(0);
+  const [viewRight, setViewRight] = useState(timeRange);
+
+  const viewTopMax = 500;
+  const viewBottomMin = 0;
 
   const colorRanges = {
     dangerouslyHigh: {
@@ -78,10 +87,30 @@ const Chart = ({timeRange, readings}) => {
     setChartHeight(width);
   }, []);
 
+  // In order to calibrate the points to the viewbox, we will need to know a scalable value for the left/right (start/end) times
+  // Solid MVP first pass suggests that time start should be ~450, and the time end ~50
+  // Readings will need to be a scalable timestamp as well
+
+  // All points most likely best handled by program with epoch/unix time
+  // The app can pull the current timestamp independently:
+    // x = Date.now();
+
+  // The readings can be done with:
+    // x = new Date(`${reading.observed_date}T${reading.observed_time}`).getTime();
+
+  // Readings can be calibrated by subtracting all timestamps by that of the timespan start
+  // We should convert milliseconds to minutes using:
+    // x = x / 60000;
+
+  // With everything now "left-grounded", we can simply divide all values by the value that takes the "now" timestamp to the desired right border (500 during init dev)
+
   return (
     <div id="chart-container">
-      <StyledSVG width={chartWidth} height={chartHeight}
-        viewBox={`0 0 ${chartWidth} ${chartHeight}`} className="chart">
+      <StyledSVG width={chartWidth} height={chartHeight} viewBox={`0 0 500 ${viewTopMax}`} className="chart">
+        <rect id="diabetes-range" x="0" y="0" width={500} height={375} fill="rgba(255,0,0,.2)"/>
+        <rect id="prediabetes-range" x="0" y="375" width={500} height={25} fill="rgba(255,255,0,0.2)"/>
+        <rect id="normalglycemia-range" x="0" y="400" width={500} height={30} fill="rgba(0,255,0,0.2)"/>
+        <rect id="hypoglycemia-range" x="0" y="430" width={500} height={70} fill="rgba(255,0,0,.2)"/>
       </StyledSVG>
     </div>
 )};
