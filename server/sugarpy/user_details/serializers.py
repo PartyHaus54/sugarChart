181a8ae8035baa9e822da54fd83ff30aaef6bf54
Joseph Peck <joe.r.peck@gmail.com>
2021-10-28 14:51:05 -0700

    Server side now responds and adds user details

--- server/sugarpy/user_details/serializers.py
+++ server/sugarpy/user_details/serializers.py
@@ -1,18 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from user_details.models import UserDetail as UserDetailModel
+from user_details.models import UserDetail as UserDetail
 
 class UserDetailSerializer(serializers.ModelSerializer):
   class Meta:
-    model = UserDetailModel
-    fields = ('user', 'age', 'weight', 'timezone')
+    model = UserDetail
+    fields = ('age', 'weight', 'timezone')
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class RegisterUserSerializer(serializers.HyperlinkedModelSerializer):
   details = UserDetailSerializer(many=False, read_only=True)
 
   class Meta:
     model = User
-    fields = ['id', 'username', 'first_name', 'last_name', 'email', 'details', 'password']
+    fields = ['username', 'password', 'details']
 
   def create(self, validated_data):
     print(validated_data)
@@ -21,5 +21,26 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
       password=validated_data['password']
     )
 
+    print(user.id)
+
     user.save()
     return user
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+  details = UserDetailSerializer(many=False, read_only=True)
+
+  class Meta:
+    model = User
+    fields = ['username', 'details']
+
+  # def create(self, validated_data):
+  #   print(validated_data)
+  #   user = User.objects.create_user(
+  #     username=validated_data['username'],
+  #     password=validated_data['password']
+  #   )
+
+  #   print(user.id)
+
+  #   user.save()
+  #   return user
